ee0b384c3562d60769af89ce6d74d80421c6c78d
jeckhart582@gmail.com <jeckhart582@gmail.com>
2018-12-06 18:16:52 -0600

    third push, got rid of my extra js files that i was messing with, commited to one now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,39 @@
 // An array that lists out potential words to pick (pls fix me)
-var computerChoice = ["banana"];
+var computerChoice = ["banana", "kiwi", "apple", "cantaloupe", "watermelon", "honeydew", "berry",];
+
+// Randomly chooses a word from the computerChoice variable
+var computerWord = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
 //An array that self loops to generate blank spots for guessing letters
 var answerArray = [];
-for (var i = 0; i < computerWord; i++) {
-    answerArray[i] = "_";
-}
-var computerWord = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+for (var i = 0; i < computerWord.length; i++) {
+    answerArray[i] = " _ ";
+    };
 
-//Variable for how many letters are remaning
+// Variable for how many letters are remaning
 var remainingLetters = computerWord.length;
 
-var userGuess = event.key;
-
-
 // Create variables that hold references to the places in the HTML where we want to display things.
-var userChoiceText = document.getElementById("userchoice-text");
+var userChoiceText = document.getElementById("userinput-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var answerArrayText = document.getElementById("answerarray-text");
-answerArrayText.textContent = answerArray;
 
-
+answerArrayText.textContent = answerArray[i];
 
 //When the user presses a key, run this fuction
+document.onkeydown = function (event) {
+    // Creates variable for which key the user pressed
+    var userGuess = event.key;
 
-    var computerWord = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    // If user pressed any key
+    if ((userGuess === "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r","s", "t", "u", "v")) {
+        answerArrayText.textContent = answerArray.join("");
 
-
-        if ((userGuess === "a") || (userGuess === "b") || (userGuess === "n")) {
-            
-            userChoiceText.textContent = "You chose: " + userGuess;
-            computerChoiceText.textContent = "The computer's word is: " + computerWord;
-            answerArrayText.textContent = answerArray.join(" _ ");
-            
-                for (var j = 0; j < computerWord.length; j++) {
-                    if (computerWord[j] === userGuess) {
-                    answerArray[j] = userGuess;
-                    remainingLetters--;
-                    }
-                }
-             }
+        for (var j = 0; j < computerWord.length; j++) {
+            if (computerWord[j] === userGuess) {
+                answerArray[j] = userGuess;
+                remainingLetters--;
+            };
+        };
+    };
+};
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,20 +1,60 @@
+var randomWords = [ "banana", "orange", "kiwi", "pineapple", "apple", "cantaloupe", "honeydew", "watermelon",
+];
+
+
+
+
+
 var userGuessedLetters = [];
+var selectedWordIndex;
+var builtWord = [];
 
 
 
 function restartGame() {
+    selectedWordIndex = Math.floor(Math.random() * (randomWords.length));
+    
     userGuessedLetters = [];
-}
+    builtWord= [];
 
-function refrestDisplay() {
+    for (var i = 0; i < randomWords[selectedWordIndex].length; i++) {
+        builtWord.push("_");
+    }
+};
+
+function refreshDisplay() {
+    var builtWordText = "";
+    for (var i = 0; i < builtWord.length; i++) {
+        builtWordText += builtWord[i];
+    }
+    
+    document.getElementById("answer").innerText= builtWord;
     document.getElementById("userGuessedLetters").innerText = userGuessedLetters;
 
-}
+};
+
+function checkGuess(letter) {
+    var positions = [];
+    
+    for (var i = 0; i < randomWords[selectedWordIndex].length; i++){
+        if(randomWords[selectedWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    if (positions.length <=0) {
+        remainingGuesses--;
+    } else {
+        for (var i =0; i <positions.length; i++) {
+            builtWord[positions[i]] = letter;
+        }
+    }
+};
 
 function makeGuess(letter) {
     if (userGuessedLetters.indexOf(letter) === -1) {
         userGuessedLetters.push(letter);
-        evaluteGuess(letter);
+        checkGuess(letter);
     }
 };
 
@@ -23,4 +63,6 @@ document.onkeydown = function(event) {
         makeGuess(event.key.toLowerCase());
         refreshDisplay();
     }
-}
+};
+
+
